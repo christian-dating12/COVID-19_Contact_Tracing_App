f5fa69a6fb4f08dbb036c9bf4af7493d29598064
chrismdating1206@gmail.com <christian.dating12>
2023-07-26 21:53:54 +0800

    add entry button and personal infos

--- Add_Entry.py
+++ Add_Entry.py
@@ -1,4 +1,9 @@
 import tkinter as tk
 
-class add_entry:
-    pass
+class add_entry(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+
+        self.personal_info_label = tk.Label(self, text=" PERSONAL INFORMATIONS ", height=2, font=("Monteserrat", 10, "bold"))
+        self.personal_info_label.place(x=300, y=50)
+        self.personal_info_label.config(bg="#800000")
--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@
 # Search Entry
 
 import tkinter as tk
-
+from Add_Entry import add_entry
 
 
 class App_Window:
@@ -15,23 +15,20 @@ class App_Window:
         self.window = tk.Tk()
         self.window.title("COVID-19 CONTACT TRACING APP")
         self.window.geometry("800x600")
-        self.window.configure(bg="#C1C1CD")
+        self.window.configure(bg="#808080")
 
         tk.Label(self.window, text="COVID-19 CONTACT TRACING APP",
-                 font=("Impact", 30, "bold"),bg="#C1C1CD", fg="#800000").pack(pady=20)
+                 font=("Impact", 30, "bold"),bg="#808080", fg="#800000").pack(pady=20)
      
-       # Create add entry button.
-        add_button = tk.Button(self.window, text="Add Entry", command=self.add_entry, height=3, bg="yellow", fg="black", font=("Arial", 10, "bold"))
-        add_button.place(x=450, y=290, width=200)
-
-        # Create search entry button.
-        search_button = tk.Button(self.window, text="Search Entry", command=self.search_entry, height=3, bg="pink", fg="black", font=("Arial", 10, "bold"))
-        search_button.place(x=450, y=360, width=200)
-
         
+        add_button = tk.Button(self.window, text="Add Entry", command=self.add_entry, height=2, bg="white", fg="black", font=("Monteserrat", 10, "bold"))
+        add_button.place(x=150, y=190, width=150)
+
         self.window.mainloop()
 
-
+    def add_entry(self):
+        frame_frame = add_entry()
+        frame_frame.place(x=0, y=0, relwidth=1, relheight=1)
     
 App_Window()        
 
