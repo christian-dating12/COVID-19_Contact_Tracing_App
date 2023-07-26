a63dcaad716e4f51fc2f2b2d036d6173c7091002
chrismdating1206@gmail.com <christian.dating12>
2023-07-26 22:37:28 +0800

    user's sex

--- Add_Entry.py
+++ Add_Entry.py
@@ -17,6 +17,19 @@ class add_entry(tk.Frame):
         self.name_entry.bind("<FocusIn>", self.clear_name_text)
         self.name_entry.config(fg="black", bg="#FFFFFF", font=("Monteserrat", 15))
 
+        self.sex = tk.Label(self, text="   Sex:   ", height=2, font=("Monteserrat", 10, "bold"))
+        self.sex.place(x=492, y=110)
+        self.sex.config(bg="#808080")
+        self.sex_entry = tk.Entry(self, width=20)
+        self.sex_entry.place(x=550, y= 115)
+        self.sex_entry.insert(0, " Male / Female")  
+        self.sex_entry.bind("<FocusIn>", self.clear_sex_text)
+        self.sex_entry.config(fg="black", bg="#FFFFFF", font=("Monteserrat", 15))
+
     def clear_name_text(self, event):
         self.name_entry.delete(0, tk.END)
         self.name_entry.config(fg="black")
+
+    def clear_sex_text(self, event):
+        self.sex_entry.delete(0, tk.END)
+        self.sex_entry.config(fg="black")
